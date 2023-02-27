fb5fe7a41d54e6d93b8160a900de2889fa35ef9f
Bizolivier <bizimunguolivier3108@gmail.com>
2023-02-27 14:38:00 +0100

    Ajoutez des fichiers projet.

---
+++ LinqWithObjects/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            string[] names = new[] { "Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed" };
+
+
+            var query4 = names.Where(name => name.Length > 4)
+                              .OrderBy(name => name.Length);
+
+            Console.WriteLine("Names longuer than four letters and order by length");
+
+            Console.WriteLine("**********************************************************");
+
+            foreach (var name in query4) {
+
+
+                Console.WriteLine(name);
+            }
+
+
+
+
+            Console.WriteLine("Deferred execution ");
+
+            //which names ends with M
+
+            var query1 = names.Where(name => name.EndsWith("m"));
+
+           foreach(var name in query1) {
+
+                Console.WriteLine(name);
+            }
+            //which names ends with M 
+            // written using Linq query comprenhension syntax
+
+            var query2 = from name in names where name.EndsWith("m")select name;
+
+            var query = names.Where(new Func<string, bool>(NameLongerThanFour));
+
+            //Optimaze solution
+
+            var query3 = names.Where(NameLongerThanFour);
+
+            foreach (var name in query) {
+
+
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("**********************************************************");
+
+
+            var query5 = names.Where(name => name.Length > 4 )
+                              .OrderBy(name => name.Length)
+                              //ThenBy ordonne par ordre alphabetic
+                              .ThenBy(name => name);
+
+            Console.WriteLine("Names longuer than four letters and order by length");
+
+            Console.WriteLine("**********************************************************");
+
+            foreach (var name in query5) {
+                
+                
+                Console.WriteLine(name);
+            }
+
+        }
+
+        static bool NameLongerThanFour(string name) {
+            return name.Length > 4;
+        }
+    }
+}
